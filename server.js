b1b28e726b29ff62f07ee4442ac9eb4b75e7620d
Yasmin Martinez <jazmartinez250716@gmail.com>
2025-11-10 15:43:45 -0600

    Actualizar a mysql2 para arreglar autenticacion

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 const jwt = require('jsonwebtoken'); 
 
