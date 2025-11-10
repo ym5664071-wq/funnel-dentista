ef836a3a9e0bd5b8be077ebf3b4255478306a3c3
Yasmin Martinez <jazmartinez250716@gmail.com>
2025-11-10 13:55:41 -0600

    Corregir puerto de BD y eliminar .env del repositorio

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
