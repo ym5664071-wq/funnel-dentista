e8af8bb3a87d30b6153301d2a6e5d05c7e7c93fe
Yasmin Martinez <jazmartinez250716@gmail.com>
2025-11-10 16:03:46 -0600

    Forzar conexión SSL a la base de datos

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: { "rejectUnauthorized": true }
 });
 
 db.connect(err => {
